b4aefb4ade4ab4106903462f53e848a966046317
Maciej Abramowski <m.abramowski@wp.pl>
2017-06-10 15:38:53 +0200

    fin

--- js/10-functions.js.js
+++ js/10-functions.js.js
@@ -24,3 +24,26 @@ var funkcja = function () {
 
 console.log(funkcja);
 console.log(funkcja());
+
+
+
+//funkcja 
+
+var krzysiek = 3500;
+var jurek = 5000;
+var jola = 7500;
+var sergio = 2500;
+
+var sumaKasy
+
+function policzSumeKasy(skladowa1, skladowa2, skladowa3, skladowa4){
+    var sumaKasy;
+    sumaKasy = skladowa1 + skladowa2 + skladowa3 + skladowa4;
+    
+    return sumaKasy;
+}
+
+sumaKasy = policzSumeKasy(krzysiek, jurek, jola, sergio);
+
+console.log( sumaKasy);
+
