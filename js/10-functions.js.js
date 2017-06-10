2c769b6c6907a673a3f96757e894262debddd08d
Maciej Abramowski <m.abramowski@wp.pl>
2017-06-10 14:43:17 +0200

    function multiply

--- js/10-functions.js.js
+++ js/10-functions.js.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function multiply(parametr1, parametr2, parametr3) {
+    
+    var result;
+    
+    result = parametr1 * parametr2 * parametr3;
+    
+    return result;
+}
+
+var wynik = multiply(3,9,10);
+
+console.log('Wynik = ' + wynik);
